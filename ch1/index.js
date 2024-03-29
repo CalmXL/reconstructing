0e5c202af8f3247798cd3c51ebb819ca2b109065
xl <xl18845898715@gmail.com>
2024-03-29 20:52:12 +0800

    reconstruct: 抽取playFor 和 amountFor

--- ch1/index.js
+++ ch1/index.js
@@ -22,27 +22,32 @@ const plays = {
 };
 
 
-function amountFor (perf, play) {
-  let thisAmount = 0;
-  switch (play.type) {
+function amountFor (aPerformance) {
+  let result = 0;
+  switch (playFor(aPerformance).type) {
     case "tragedy":
       thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        thisAmount += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
       thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      thisAmount += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
+
+  return result;
 }
 
+function playFor (aPerformance) {
+  return plays[aPerformance.playID]
+}
 
 function statement(invoice, plays) {
   let totalAmount = 0;
@@ -54,14 +59,13 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
